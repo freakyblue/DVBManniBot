dda0a2b9d89aef094b24c54be516cebb88cc2f88
Jonny Seitz <github@jonny-seitz.de>
2017-06-23 00:16:12 +0200

    Max length of 100 in index.php

--- index.php
+++ index.php
@@ -78,8 +78,9 @@ if ($chatId) {  //to hide warnings from website
       if (count(explode(' ', $inputMsg)) == 1)
         sendMsg($chatId, $resp['contact_no_input'],'');
       else {
-        sendMsg($contactId, 'from @'.$senderUsername.' ('.$senderFirstName.' '.$senderLastName.') '.$chatId.PHP_EOL.
-          $inputMsg,'');
+        sendMsg($contactId, 'from @'.$senderUsername.' ('.$senderFirstName.' '.$senderLastName.') '.
+          $chatId.PHP_EOL.$inputMsg,''
+        );
         sendMsg($chatId, $resp['contact_thx'],'');
       }//else
       break;
